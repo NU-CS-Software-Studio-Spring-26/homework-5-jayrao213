dee1042fcbf4475ed475338f764ced537c3ed050
Jay Rao <jayjohnrao@gmail.com>
2026-05-26 22:23:49 -0500

    feat: implement high priority toggle via Rails Turbo Streams

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :todos
+  resources :todos do
+    collection do
+      post :reorder
+    end
+    member do
+      patch :toggle_priority
+    end
+  end
   get '/hello', to: 'todos#hello'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
